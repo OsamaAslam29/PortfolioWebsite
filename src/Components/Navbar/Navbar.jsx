54370e934f6a5799b57b6aaf1b020a63caeba38c
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-20 00:34:23 +0500

    index.css added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav_container'>
+      <div className="logo">Sufyab</div>
+      <div className="menu"></div>
+    </div>
+  )
+}
+
+export default Navbar
