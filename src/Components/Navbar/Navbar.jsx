a5018484131dc071f0daab1a66a6d50c0d8d1025
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-20 22:44:32 +0500

    add navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
     <div className='nav_container'>
-      <div className="logo">Sufyab</div>
-      <div className="menu"></div>
+      <div className="logo">Sufyan</div>
+      <div className="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Experience</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact Me</a>
+      </div>
     </div>
   )
 }
