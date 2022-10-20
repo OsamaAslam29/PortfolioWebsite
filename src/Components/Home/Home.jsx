5a5936f4875e4242ba281fc3be4a52ef00fef321
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-21 00:15:10 +0500

    add animation

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className='home_container'>
+        <div className="bio_data">
+            <h5>Hi, my name is</h5>
+            <h1>Abu Sufyan</h1>
+            <h1 className='bio'>I build things for the web.</h1>
+        </div>
+        <div className="bio_animation">
+            <iframe title="background-3d" src="https://my.spline.design/awesomefolio-0c39efc3de701ba97be395cc128e4e2f/" frameBorder="0" height="100%" className="jss83"></iframe>
+            <div className="black_box"></div>
+        </div>
+    </div>    
+  )
+}
+
+export default Home
