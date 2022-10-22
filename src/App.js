bd50e6284af9963d1cb8c434c2a544d61507938b
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-22 21:44:24 +0500

    about section start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './Components/About/About';
 import Home from './Components/Home/Home';
 import Navbar from "./Components/Navbar/Navbar.jsx"
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar/>
       <Home/>
+      <About/>
     </>
   );
 }
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './About.scss'
 
 const About = () => {
   return (
     <div className='about_container'>
-      
+      <div className="flex_about">
+        <div className="left_border"></div>
+        <div className="content">About</div>
+        <div className="right_border"></div>
+      </div>
     </div>
   )
 }
