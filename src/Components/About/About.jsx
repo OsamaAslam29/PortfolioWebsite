f46ba413b81cb81e7dd64a54a2ed86c6141f7315
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-25 00:53:12 +0500

    deploy

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,17 +8,17 @@ const About = () => {
   return (
     <div className='about_container'>
       <div className="divider_line">
-          <div className="left_border"></div>
-          <div className="content">About</div>
-          <div className="right_border"></div>
+        <div className="left_border"></div>
+        <div className="content">About</div>
+        <div className="right_border"></div>
       </div>
 
       <div className="flex_about">
-          <div className="skils_container">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia delectus sit assumenda culpa perspiciatis odio neque recusandae eius. Asperiores fuga dignissimos omnis tempore nostrum repellendus optio, eos totam natus illum, reiciendis perspiciatis corporis accusantium eligendi esse est rem excepturi perferendis. Dolor deleniti itaque, voluptates vitae cupiditate possimus dolorem nam repellendus!</p>
-           
-            <div className="flex_skills">   
-              <div className="basic_skills">
+        <div className="skils_container">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia delectus sit assumenda culpa perspiciatis odio neque recusandae eius. Asperiores fuga dignissimos omnis tempore nostrum repellendus optio, eos totam natus illum, reiciendis perspiciatis corporis accusantium eligendi esse est rem excepturi perferendis. Dolor deleniti itaque, voluptates vitae cupiditate possimus dolorem nam repellendus!</p>
+
+          <div className="flex_skills">
+            <div className="basic_skills">
               <div className="skill_name">
                 <div className="skill">HTML</div>
                 <Box sx={{ width: '80%' }}>
@@ -49,8 +49,8 @@ const About = () => {
                   <LinearProgress variant="buffer" value={90} />
                 </Box>
               </div>
-              </div>
-              <div className="skills">
+            </div>
+            <div className="skills">
               <div className="skill_name">
                 <div className="skill">Bootstrap</div>
                 <Box sx={{ width: '80%' }}>
@@ -81,14 +81,14 @@ const About = () => {
                   <LinearProgress variant="buffer" value={85} />
                 </Box>
               </div>
-              </div>
             </div>
+          </div>
 
 
-          </div>
-          <div className="image_container">
-            <img src={me} alt="" />
-          </div>
+        </div>
+        <div className="image_container">
+          <img src={me} alt="" />
+        </div>
       </div>
 
     </div>
