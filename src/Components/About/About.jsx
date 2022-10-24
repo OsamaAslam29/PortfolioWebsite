2644d3fbca6854807361b375ea42e412cc17cc53
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-24 23:27:04 +0500

    add spinner

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './About.scss'
+import me from '../../Assets/me.png'
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const About = () => {
   return (
@@ -9,7 +12,18 @@ const About = () => {
         <div className="content">About</div>
         <div className="right_border"></div>
       </div>
-      <h1>This is About page</h1>
+
+      <div className="flex_about">
+        <div className="skils_container">
+          <Box sx={{ width: '100%' }}>
+            <LinearProgress variant="buffer" value={50} />
+          </Box>
+        </div>
+        <div className="image_container">
+          <img src={me} alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
