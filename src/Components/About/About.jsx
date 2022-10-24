cf3fbc8f17b8bb8631a2c8573e6328e958bf3b7e
OslamAslam29 <osamaaslam029@gmail.com>
2022-10-25 00:45:48 +0500

    about pages

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,20 +8,87 @@ const About = () => {
   return (
     <div className='about_container'>
       <div className="divider_line">
-        <div className="left_border"></div>
-        <div className="content">About</div>
-        <div className="right_border"></div>
+          <div className="left_border"></div>
+          <div className="content">About</div>
+          <div className="right_border"></div>
       </div>
 
       <div className="flex_about">
-        <div className="skils_container">
-          <Box sx={{ width: '100%' }}>
-            <LinearProgress variant="buffer" value={50} />
-          </Box>
-        </div>
-        <div className="image_container">
-          <img src={me} alt="" />
-        </div>
+          <div className="skils_container">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia delectus sit assumenda culpa perspiciatis odio neque recusandae eius. Asperiores fuga dignissimos omnis tempore nostrum repellendus optio, eos totam natus illum, reiciendis perspiciatis corporis accusantium eligendi esse est rem excepturi perferendis. Dolor deleniti itaque, voluptates vitae cupiditate possimus dolorem nam repellendus!</p>
+           
+            <div className="flex_skills">   
+              <div className="basic_skills">
+              <div className="skill_name">
+                <div className="skill">HTML</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={95} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">CSS/Saas</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">JavaScript</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={70} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">React Js</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={80} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">Redux/Redux Toolkit</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              </div>
+              <div className="skills">
+              <div className="skill_name">
+                <div className="skill">Bootstrap</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">Material UI</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">Git Hub</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">Bit Bucket</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={90} />
+                </Box>
+              </div>
+              <div className="skill_name">
+                <div className="skill">Api Integration</div>
+                <Box sx={{ width: '80%' }}>
+                  <LinearProgress variant="buffer" value={85} />
+                </Box>
+              </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="image_container">
+            <img src={me} alt="" />
+          </div>
       </div>
 
     </div>
